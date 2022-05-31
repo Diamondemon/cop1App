7c3ddfd129936415bfddfb3a0b6a5e2a1e0c31d4
QuentinN42 <quentin@lieumont.fr>
2022-05-31 17:59:59 +0200

    fix: WIP working
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- back/app/api/login.py
+++ back/app/api/login.py
@@ -1,6 +1,5 @@
 from fastapi import Depends, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
 
 from app.login import login, decode_token
 from app.interfaces.main import BearerToken, User
@@ -13,8 +12,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.post("/token")  # type: ignore
-async def login(token: BearerToken = Depends(login)) -> BearerToken:
-    return token
+async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> BearerToken:
+    return login(form_data)
 
 
 @app.get("/users/me")
