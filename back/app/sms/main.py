5cd4ece40449522a29debc0fbb90d25ded8ef51c
QuentinN42 <quentin@lieumont.fr>
2022-07-16 18:35:33 +0200

    feat: twilo
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- back/app/sms/main.py
+++ back/app/sms/main.py
@@ -1,8 +1,19 @@
+from os import getenv
 from app.logger import logger
+# from twilio.rest import Client
+
+# account_sid = getenv('TWILIO_ACCOUNT_SID')
+# auth_token = getenv('TWILIO_AUTH_TOKEN')
+# client = Client(account_sid, auth_token)
 
 
 def generate_and_send_token(phone: str) -> str:
+    """Generate the token and sent it via twilo"""
     logger.info('Generating token for user : %s', phone)
     token = '123456'
-    logger.info('Token is : %s', token)
+    # client.messages.create(
+    #     body=f'Your code is ; {token}',
+    #     from_='+15017122661',
+    #     to=phone
+    # )
     return token
