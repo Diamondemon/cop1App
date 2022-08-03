745c6b16e15defeca924efb9042fc5db4d073a59
Julien Marzal <julien.marzal@telecom-paris.fr>
2022-08-03 16:30:11 +0200

    [WIP] Default static QR code implementation done

--- cop1/lib/ui/event_page.dart
+++ cop1/lib/ui/event_page.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
+
+import 'package:cop1/data/session_data.dart';
+import 'package:cop1/utils/user_profile.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+import '../utils/cop1_event.dart';
 
 class EventPage extends StatefulWidget {
   const EventPage({Key? key, required this.eventId}) : super(key: key);
@@ -11,8 +19,140 @@ class EventPage extends StatefulWidget {
 class _EventPageState extends State<EventPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    final Cop1Event event = session(context).getEvent(widget.eventId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(event.title),
+        actions: session(context).isConnected? [_buildQRCodeButton(context)] : [],
+      ),
+      body: ListView(
+          children: [
+            Text(event.title, style: Theme.of(context).textTheme.headlineSmall,),
+            _buildImage(context, event),
+            const Text.rich(
+              TextSpan(
+                children: [
+                  WidgetSpan(
+                    alignment: PlaceholderAlignment.top,
+                    child: Icon(Icons.calendar_month, size: 14),
+                  ),
+                  TextSpan(
+                    text: " Calendrier",
+                  ),
+                ],
+              ),
+            ),
+            TextButton(onPressed: event.addToCalendar,
+                child: Text(
+                    "${event.date} ${event.hour}",
+                    style: const TextStyle(fontSize: 12)
+                )
+            ),
+            const Text.rich(
+              TextSpan(
+                children: [
+                  WidgetSpan(
+                    alignment: PlaceholderAlignment.top,
+                    child: Icon(CupertinoIcons.location, size: 14),
+                  ),
+                  TextSpan(
+                    text: " Lieu",
+                  ),
+                ],
+              ),
+            ),
+            TextButton(onPressed: event.lookoutLocationOnMaps, child:Text(event.location, style: const TextStyle(fontSize: 12))),
+            const Text.rich(
+              TextSpan(
+                children: [
+                  WidgetSpan(
+                    alignment: PlaceholderAlignment.top,
+                    child: Icon(CupertinoIcons.timer, size: 14),
+                  ),
+                  TextSpan(
+                    text: " Durée",
+                  ),
+                ],
+              ),
+            ),
+            Text(event.duration),
+            const Text.rich(
+              TextSpan(
+                children: [
+                  WidgetSpan(
+                    alignment: PlaceholderAlignment.top,
+                    child: Icon(CupertinoIcons.info, size: 14),
+                  ),
+                  TextSpan(
+                    text: " Informations complémentaires",
+                  ),
+                ],
+              ),
+            ),
+            Text(event.description),
+          ],
+        ),
+    );
+  }
 
+
+  Widget _buildImage(BuildContext context, Cop1Event event){
+    try {
+      return Image.network(event.imageLink, fit: BoxFit.fill);
+    }
+    on SocketException {
+      return const Icon(Icons.image_not_supported);
+    }
+  }
+
+
+  Widget _buildQRCodeButton(BuildContext context){
+    return FutureBuilder(
+      future: session(context).user,
+      builder: (BuildContext context, AsyncSnapshot<UserProfile?> snapshot){
+        if (snapshot.connectionState == ConnectionState.done){
+          if (snapshot.hasError && snapshot.error is! SocketException){
+            //return Text(snapshot.error.toString());
+            return Container();
+          }
+          else if (snapshot.data==null){
+            return Container();
+          }
+          else {
+            final UserProfile user = snapshot.data as UserProfile;
+            return IconButton(
+              icon: const Icon(Icons.qr_code),
+              onPressed: ()=>_displayQRCodeAlert(context, "12345"),
+            );
+          }
+        }
+        else {
+          return Container();
+        }
+      }
+    );
+  }
+
+  void _displayQRCodeAlert(BuildContext context, String code){
+    showDialog(
+        context: context,
+        builder:
+          (BuildContext alertContext){
+            return _buildQRCodeAlert(alertContext, code);
+          });
+  }
+
+
+  Dialog _buildQRCodeAlert(BuildContext context, String code){
+    return Dialog(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.width*0.8,
+        width: MediaQuery.of(context).size.width*0.8,
+        child: QrImage(
+          data: code,
+          version: QrVersions.auto,
+        )
+      )
     );
   }
 }
--- cop1/lib/ui/event_tile.dart
+++ cop1/lib/ui/event_tile.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:cop1/data/session_data.dart';
+import 'package:cop1/ui/event_page.dart';
 import 'package:cop1/utils/cop1_event.dart';
 import 'package:cop1/ui/subscribe_button.dart';
 import 'package:flutter/cupertino.dart';
@@ -17,80 +19,83 @@ class EventTile extends StatefulWidget {
 class _EventTileState extends State<EventTile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height/4,
-      decoration: BoxDecoration(
-        border: Border.symmetric(
-            horizontal: BorderSide(color: Theme.of(context).primaryColor, width:3.0)
-        ),
-      ),
-      padding: const EdgeInsets.only(top: 10.0, bottom: 5.0, right:10.0, left:10.0),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                    widget.event.title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const Spacer(),
-                _buildImage(context),
-                const Spacer(),
-              ]
-            )
+    return InkWell(
+      onTap: ()=> _openEventPage(context),
+      child: Container(
+          height: MediaQuery.of(context).size.height/4,
+          decoration: BoxDecoration(
+            border: Border.symmetric(
+                horizontal: BorderSide(color: Theme.of(context).primaryColor, width:3.0)
+            ),
           ),
-          Expanded(
-            flex: 2,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          padding: const EdgeInsets.only(top: 10.0, bottom: 5.0, right:10.0, left:10.0),
+          child: Row(
               children: [
-                const Text.rich(
-                  TextSpan(
-                    children: [
-                      WidgetSpan(
-                        alignment: PlaceholderAlignment.top,
-                        child: Icon(Icons.calendar_month, size: 14),
-                      ),
-                      TextSpan(
-                        text: " Calendrier",
-                      ),
-                    ],
-                  ),
+                Expanded(
+                    flex: 3,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.event.title,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          const Spacer(),
+                          _buildImage(context),
+                          const Spacer(),
+                        ]
+                    )
                 ),
-                TextButton(onPressed: widget.event.addToCalendar,
-                  child: Text(
-                    "${widget.event.date} ${widget.event.hour}",
-                    style: const TextStyle(fontSize: 12)
-                  )
-                ),
-                const Text.rich(
-                  TextSpan(
-                    children: [
-                      WidgetSpan(
-                        alignment: PlaceholderAlignment.top,
-                        child: Icon(CupertinoIcons.location, size: 14),
-                      ),
-                      TextSpan(
-                        text: " Lieu",
-                      ),
-                    ],
-                  ),
-                ),
-                TextButton(onPressed: widget.event.lookoutLocationOnMaps, child:Text(widget.event.location, style: const TextStyle(fontSize: 12))),
-                const Spacer(),
-                Expanded(flex: 2, child: Center(
-                  child: SubscribeButton(event: widget.event),
-                )
+                Expanded(
+                    flex: 2,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text.rich(
+                            TextSpan(
+                              children: [
+                                WidgetSpan(
+                                  alignment: PlaceholderAlignment.top,
+                                  child: Icon(Icons.calendar_month, size: 14),
+                                ),
+                                TextSpan(
+                                  text: " Calendrier",
+                                ),
+                              ],
+                            ),
+                          ),
+                          TextButton(onPressed: widget.event.addToCalendar,
+                              child: Text(
+                                  "${widget.event.date} ${widget.event.hour}",
+                                  style: const TextStyle(fontSize: 12)
+                              )
+                          ),
+                          const Text.rich(
+                            TextSpan(
+                              children: [
+                                WidgetSpan(
+                                  alignment: PlaceholderAlignment.top,
+                                  child: Icon(CupertinoIcons.location, size: 14),
+                                ),
+                                TextSpan(
+                                  text: " Lieu",
+                                ),
+                              ],
+                            ),
+                          ),
+                          TextButton(onPressed: widget.event.lookoutLocationOnMaps, child:Text(widget.event.location, style: const TextStyle(fontSize: 12))),
+                          const Spacer(),
+                          Expanded(flex: 2, child: Center(
+                            child: SubscribeButton(event: widget.event),
+                          )
+                          )
+                        ]
+                    )
                 )
               ]
-            )
           )
-        ]
-      )
+      ),
     );
   }
 
@@ -101,6 +106,10 @@ class _EventTileState extends State<EventTile> {
     on SocketException {
       return const Icon(Icons.image_not_supported);
     }
+  }
+
+  void _openEventPage(BuildContext context){
+    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context)=>EventPage(eventId: widget.event.id)));
   }
 
 }
