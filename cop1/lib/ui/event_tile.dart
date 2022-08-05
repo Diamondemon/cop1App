817de222e645fab270bcb234a20cd0282cac4c67
Julien Marzal <julien.marzal@telecom-paris.fr>
2022-08-05 08:47:06 +0200

    [WIP] Fix: take text scale factor into account for image too

--- cop1/lib/ui/event_tile.dart
+++ cop1/lib/ui/event_tile.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:cop1/ui/event_page.dart';
 import 'package:cop1/utils/cop1_event.dart';
@@ -21,86 +22,86 @@ class _EventTileState extends State<EventTile> {
     return InkWell(
       onTap: ()=> _openEventPage(context),
       child: Container(
-          height: (MediaQuery.of(context).size.height/4)*MediaQuery.of(context).textScaleFactor,
-          decoration: BoxDecoration(
-            border: Border.symmetric(
-                horizontal: BorderSide(color: Theme.of(context).primaryColor, width:3.0)
-            ),
+        height: (MediaQuery.of(context).size.height/4)*MediaQuery.of(context).textScaleFactor,
+        decoration: BoxDecoration(
+          border: Border.symmetric(
+              horizontal: BorderSide(color: Theme.of(context).primaryColor, width:3.0)
           ),
-          padding: const EdgeInsets.only(top: 10.0, bottom: 5.0, right:10.0, left:10.0),
-          child: Row(
-              children: [
-                Expanded(
-                    flex: 3,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.event.title,
-                            style: Theme.of(context).textTheme.titleMedium,
-                          ),
-                          const Spacer(),
-                          _buildImage(context),
-                          const Spacer(),
-                        ]
+        ),
+        padding: const EdgeInsets.only(top: 10.0, bottom: 5.0, right:10.0, left:10.0),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.event.title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const Spacer(),
+                    _buildImage(context),
+                    const Spacer(),
+                  ]
+              )
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text.rich(
+                    TextSpan(
+                      children: [
+                        WidgetSpan(
+                          alignment: PlaceholderAlignment.top,
+                          child: Icon(Icons.calendar_month, size: 14),
+                        ),
+                        TextSpan(
+                          text: " Calendrier",
+                        ),
+                      ],
+                    ),
+                  ),
+                  TextButton(onPressed: widget.event.addToCalendar,
+                      child: Text(
+                          "${widget.event.date} ${widget.event.hour}",
+                          style: const TextStyle(fontSize: 12)
+                      )
+                  ),
+                  const Text.rich(
+                    TextSpan(
+                      children: [
+                        WidgetSpan(
+                          alignment: PlaceholderAlignment.top,
+                          child: Icon(CupertinoIcons.location, size: 14),
+                        ),
+                        TextSpan(
+                          text: " Lieu",
+                        ),
+                      ],
+                    ),
+                  ),
+                  TextButton(onPressed: widget.event.lookoutLocationOnMaps, child:Text(widget.event.location, style: const TextStyle(fontSize: 12))),
+                  const Spacer(),
+                  Expanded(flex: 2, child: Center(
+                      child: SubscribeButton(event: widget.event),
                     )
-                ),
-                Expanded(
-                    flex: 2,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text.rich(
-                            TextSpan(
-                              children: [
-                                WidgetSpan(
-                                  alignment: PlaceholderAlignment.top,
-                                  child: Icon(Icons.calendar_month, size: 14),
-                                ),
-                                TextSpan(
-                                  text: " Calendrier",
-                                ),
-                              ],
-                            ),
-                          ),
-                          TextButton(onPressed: widget.event.addToCalendar,
-                              child: Text(
-                                  "${widget.event.date} ${widget.event.hour}",
-                                  style: const TextStyle(fontSize: 12)
-                              )
-                          ),
-                          const Text.rich(
-                            TextSpan(
-                              children: [
-                                WidgetSpan(
-                                  alignment: PlaceholderAlignment.top,
-                                  child: Icon(CupertinoIcons.location, size: 14),
-                                ),
-                                TextSpan(
-                                  text: " Lieu",
-                                ),
-                              ],
-                            ),
-                          ),
-                          TextButton(onPressed: widget.event.lookoutLocationOnMaps, child:Text(widget.event.location, style: const TextStyle(fontSize: 12))),
-                          const Spacer(),
-                          Expanded(flex: 2, child: Center(
-                            child: SubscribeButton(event: widget.event),
-                          )
-                          )
-                        ]
-                    )
-                )
-              ]
-          )
+                  )
+                ]
+              )
+            )
+          ]
+        )
       ),
     );
   }
 
   Widget _buildImage(BuildContext context){
     try {
-      return Image.network(widget.event.imageLink, fit: BoxFit.fill, height: MediaQuery.of(context).size.height/7);
+      return Image.network(widget.event.imageLink, fit: BoxFit.fill, height: MediaQuery.of(context).size.height/7*min(1, MediaQuery.of(context).textScaleFactor));
     }
     on SocketException {
       return const Icon(Icons.image_not_supported);
