7bf324d1836fa109bf1711eac67a6103c0f44a75
Julien Marzal <julien.marzal@telecom-paris.fr>
2022-08-26 02:13:19 +0200

    SnackBar with timer added

--- cop1/lib/utils/connected_widget_state.dart
+++ cop1/lib/utils/connected_widget_state.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 import '../common.dart';
 
+const Duration _snackBarDisplayDuration = Duration(milliseconds: 4000);
+
 class ConnectedWidgetState {
   static void displayConnectionAlert(BuildContext context){
     showDialog(
@@ -49,4 +51,55 @@ class ConnectedWidgetState {
       actions: [discardButton],
     );
   }
+
+  static SnackBar timedSnackBar({
+    Key? key,
+    required Widget child,
+    Color? backgroundColor,
+    double? elevation,
+    EdgeInsetsGeometry? margin,
+    EdgeInsetsGeometry? padding,
+    double? width,
+    ShapeBorder? shape,
+    SnackBarBehavior? behavior,
+    SnackBarAction? action,
+    Duration duration = _snackBarDisplayDuration,
+    Animation<double>? animation,
+    void Function()? onVisible,
+    DismissDirection dismissDirection = DismissDirection.down,
+    Clip clipBehavior = Clip.hardEdge
+  }){
+    return SnackBar(
+      key: key,
+      backgroundColor: backgroundColor,
+      elevation: elevation,
+      margin: margin,
+      padding: padding,
+      width: width,
+      shape: shape,
+      behavior: behavior,
+      action: action,
+      duration: duration,
+      animation: animation,
+      onVisible: onVisible,
+      dismissDirection: dismissDirection,
+      clipBehavior: clipBehavior,
+      content: ListView(
+        shrinkWrap: true,
+        children: [
+         child,
+         const SizedBox(height: 5.0),
+         TweenAnimationBuilder(
+             tween: Tween<double>(begin: duration.inMilliseconds *1.0, end: 0),
+             duration: duration,
+             builder: (BuildContext context, double progress, _) {
+               return LinearProgressIndicator(
+                 value: progress/duration.inMilliseconds,
+               );
+             })
+       ],
+      ),
+    );
+  }
+
 }
