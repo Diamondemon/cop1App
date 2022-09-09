685b55a08435c59332dd02c251a594410617d1de
QuentinN42 <quentin@lieumont.fr>
2022-09-09 17:02:15 +0200

    feat: sentry
    Signed-off-by: QuentinN42 <quentin@lieumont.fr>

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -1,5 +1,14 @@
+from os import getenv
+
 from dotenv import load_dotenv
+import sentry_sdk
+
+
 load_dotenv()
+
+
+if (dsn := getenv('SENTRY_DSN')):
+    sentry_sdk.init(dsn=dsn, traces_sample_rate=1.0)
 
 
 def app():
