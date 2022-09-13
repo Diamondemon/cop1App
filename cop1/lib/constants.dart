4b62c98bd8e902579bd462b1dcbe84048ba88fd5
Julien Marzal <julien.marzal@telecom-paris.fr>
2022-09-13 10:04:50 +0200

    Changed sentry dsn + little cleaning

--- cop1/lib/constants.dart
+++ cop1/lib/constants.dart
@@ -10,15 +10,5 @@ const Color radioButtonColor = Colors.blue;
 
 const String apiURL = "https://je222004.rezel.net"; // root url of the API
 
-const String sentryDsn = 'https://6c7c9a6f8392454f819e2e39856caf40@o1363652.ingest.sentry.io/6656629';
+const String sentryDsn = 'https://8b058473fa0b49c8b80de9da9a8c08f1@o1402293.ingest.sentry.io/6734401';
 const double sentryCaptureRate = 1.0;
-
-BoxDecoration buttonDecoration() => const BoxDecoration(
-  color: Colors.black12,
-  border: Border(
-    top: BorderSide(color: Colors.black, width: 2),
-    bottom: BorderSide(color: Colors.black, width: 6),
-    left: BorderSide(color: Colors.black, width: 2),
-    right: BorderSide(color: Colors.black, width: 2),
-  ),
-);
